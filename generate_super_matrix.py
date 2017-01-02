8b86b3bda78a872d5ab6f71dea6179af5920165c
masakistan <sfujimoto@gmail.com>
2017-01-02 00:22:14 -0700

    updated generate super matrix

--- generate_super_matrix.py
+++ generate_super_matrix.py
@@ -26,6 +26,9 @@ def main( args ):
                         align_len = len( seq )
                         added_specs.append( head )
                     head = line.strip()[ 1 : ]
+
+                    if args.delimiter:
+                        head = head[ : head.find( args.delimiter ) ]
                     seq = ""
                 else:
                     seq += line.strip()
@@ -36,8 +39,18 @@ def main( args ):
                 if spec not in added_specs:
                     all_seqs[ spec ] += "-" * align_len
 
+    if args.translate_names is not None:
+        names = {}
+        with open( args.translate_names, 'r' ) as fh:
+            for line in fh:
+                line = line.strip().split()
+                names[ line[ 0 ] ] = line[ 1 ]
+
     for spec, seq in all_seqs.iteritems():
-        print ">" + spec
+        if args.translate_names is not None:
+            print ">" + names[ spec ]
+        else:
+            print ">" + spec
         print seq
 
 
@@ -54,6 +67,16 @@ if __name__ == "__main__":
             type = str,
             required = True,
             help = "List of paths to all aligned fasta files for concatenation."
+            )
+    parser.add_argument( "--delimiter",
+            type = str,
+            required = False,
+            help = "delimitting character to search for in header lines. Will only grab string contents up to the delimitter."
+            )
+    parser.add_argument( "--translate_names",
+            type = str,
+            required = False,
+            help = "Table of name translations for the sequences. Tab-delimited, Name in alignment files then replacement name."
             )
 
     args = parser.parse_args()
